6104da68e8c9bfe87a21f9fbe7206b3cacc983dc
roeme <roeme@devnull.tld>
2018-12-27 16:17:01 +0100

    move user aliases out as well

--- shgit.sh
+++ shgit.sh
@@ -124,27 +124,7 @@ for cmd_alias_entry in "${_sh_cmd_aliases[@]}"; do
   alias $cmd="$cmd_alias"
 done
 _shgit_init_msg "Done setting up shell aliases."
-_shgit_init_msg "Loading your pre-defined git aliases"
-shell_keywords=( $(compgen -k) )
-
-eval "$(
-    git config --get-regexp 'alias\..*' |
-    sed 's/^alias\.//'                  |
-    while read key command
-    do
-      if in_array shell_keywords $key; then
-        [[ "${_shgit_suppress_keyword_alert:-,,}" = true ]] ||
-          _shgit_warn_msg "Your git alias '$key' is a shell keyword. This usually results in much funkiness, and hence is available as 'git $key'."
-        # By simply skipping here, we offload the alias interpretation to git.
-        continue
-      fi
-      if expr -- "$command" : '!' >/dev/null
-      then echo "alias $key='git $key'"
-      else echo "alias $key=\"${alias_cmd_prefix} git $command\""
-      fi
-    done
-  )"
-
+_shgit_load_user_aliases
 _shgit_setup_palette
 
 _shgit_init_msg "Setting up prompt hook..."
