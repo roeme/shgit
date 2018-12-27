cc3d0d7b0c27b6b688a71cc0b5f5a859f24769db
roeme <roeme@devnull.tld>
2018-12-27 16:20:06 +0100

    documentation

--- lib/user_aliases.sh
+++ lib/user_aliases.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# Loads user git aliases. As per their very nature, we don't do anything
+# fancy with them (as opposed to other commands) and simply pass them on to
+# git. However, since we let bash interpret these, we must make sure not to
+# clash with shell keywords. If there are conflicts, warn the user and prefix
+# the command.
+#
 function _shgit_load_user_aliases() {
   _shgit_init_msg "Loading your pre-defined git aliases"
   shell_keywords=( $(compgen -k) )
