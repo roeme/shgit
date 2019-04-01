d48ca0b6d3171aa1c715fedbd14516c0d2e7ecb7
roeme <roeme@devnull.tld>
2019-04-01 11:30:19 +0200

    first iteration of checkout completion

---
+++ completions/checkout.sh
@@ -0,0 +1,13 @@
+#shellcheck shell=bash
+_gitcmpl_checkout() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="-f"
+  if [[ ${cur} == -* ]] ; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  else
+    COMPREPLY=( $(compgen -W "${_shgit_branches[*]##*/}" -- ${cur}) )
+  fi
+}
